71656e2acb2e315ae07208af111da6a622afed6a
Diogo Pereira <diogo.pereira@tecnico.ulisboa.pt>
2019-10-14 02:48:23 +0100

    lab2: implement temperature debouncing / refactor

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -6,36 +6,39 @@
  * Pedro Carlos  (87556)
  */
 
-/* When DEBUG is defined, the values of the sensors are printed
- * to the serial port in the following order:
- *     temperature  rotation  light
- * Use the Arduino IDE Serial Plotter to visualize these values.
+
+/* When DEBUG is defined, these values are printed to the serial port:
+ *     blink_period  brightness  temperature
  */
 #define DEBUG
-#ifdef DEBUG
-    #define print(x) Serial.print(x);
-    #define println(x) Serial.println(x);
-#else
-    #define print(x)
-    #define println(x)
-#endif
 
 
+// Identification of the analog inputs (sensors)
 const byte TEMPERATURE_SENSOR = A0;
 const byte LIGHT_SENSOR       = A1;
 const byte POTENTIOMETER      = A3;
 
+// Identification of the digital outputs (actuators)
 const byte GREEN_LED  = 2;
 const byte RED_LED    = 3;
 const byte YELLOW_LED = 4;
 
-const word MIN_BLINK_RATE = 200;
-const word MAX_BLINK_RATE = 2000;
+// The range of the blinking period, depending on the potentiometer value
+const word MIN_BLINK_PERIOD = 200;
+const word MAX_BLINK_PERIOD = 2000;
 
-const int TEMPERATURE_THRESHOLD = 200;
+// The temperature above which the yellow LED turns on
+const float TEMPERATURE_THRESHOLD = 25.0;
+// How long a temperature must be continuously observed above
+// or below the threshold for the LED to switch states
+const word TEMPERATURE_DEBOUNCE_TIME = 20;
 
 
 void setup() {
+#ifdef DEBUG
+    Serial.begin(9600);
+#endif
+
     // Initialize the outputs
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(RED_LED, LOW);
@@ -44,53 +47,50 @@ void setup() {
     pinMode(GREEN_LED, OUTPUT);
     pinMode(RED_LED, OUTPUT);
     pinMode(YELLOW_LED, OUTPUT);
-
-#ifdef DEBUG
-    Serial.begin(9600);
-#endif
 }
 
 void loop() {
-    handle_temperature();
-    print(" ");
-    handle_rotation();
-    print(" ");
-    handle_light();
+    // Each of the sensor reading functions takes ~112 us,
+    // which is pretty much all spent in analogRead().
+    int blink_period = rotation_sensor();
+    byte brightness = brightness_sensor();
+    float temperature = temperature_sensor();
 
-    println();
+    // The actuator functions take negligible time,
+    // compared to the sensor functions.
+    rotation_actuator(blink_period);   // 2-5 us
+    brightness_actuator(brightness);   // 8 us
+    temperature_actuator(temperature); // 7-9 us
+
+#ifdef DEBUG
+    Serial.print(blink_period);
+    Serial.print(" ");
+    Serial.print(brightness);
+    Serial.print(" ");
+    Serial.println(temperature);
+#endif
 }
 
-void handle_temperature() {
-    // Read the temperature sensor value
-    int value = analogRead(TEMPERATURE_SENSOR);
-    print(value);
 
-    // Convert the sensor value to a temperature (in degrees Celsius)
-    float t = (((value / 1024.0) * 5.0) - 0.5) * 100.0;
+int rotation_sensor() {
+    // Read the rotation value
+    int value = analogRead(POTENTIOMETER);
 
-    if (t > TEMPERATURE_THRESHOLD) {
-        digitalWrite(YELLOW_LED, HIGH);
-    } else {
-        digitalWrite(YELLOW_LED, LOW);
-    }
+    // Convert the rotation value to a blinking rate
+    int blink_period = map(value, 0, 1023, MIN_BLINK_PERIOD, MAX_BLINK_PERIOD);
+
+    return blink_period;
 }
 
-void handle_rotation() {
+void rotation_actuator(int blink_period) {
     // Current LED state
     static byte led_state = LOW;
     // Last time (in milliseconds) that the LED state was toggled
     static unsigned long toggle_time = 0;
 
-    // Read the rotation value
-    int value = analogRead(POTENTIOMETER);
-    print(value);
-
-    // Convert the rotation value to a blinking rate
-    word blink_rate = map(value, 0, 1023, MIN_BLINK_RATE, MAX_BLINK_RATE);
-
     // Check if enough time has passed to toggle the LED state
     unsigned long current_time = millis();
-    if (current_time > toggle_time + blink_rate) {
+    if (current_time > toggle_time + blink_period) {
         // More time has passed than the blink rate; toggle LED state
         led_state = (led_state == LOW) ? HIGH : LOW;
         digitalWrite(GREEN_LED, led_state);
@@ -100,13 +100,63 @@ void handle_rotation() {
     }
 }
 
-void handle_light() {
+
+byte brightness_sensor() {
     // Read the light sensor value
     int value = analogRead(LIGHT_SENSOR);
-    print(value);
 
     // Map the light sensor value to a brightness level
     byte brightness = map(value, 0, 1023, 0, 255);
 
+    return brightness;
+}
+
+void brightness_actuator(int brightness) {
+    // Use PWM to control the LED brightness
     analogWrite(RED_LED, brightness);
+}
+
+
+float temperature_sensor() {
+    // Read the temperature sensor value
+    int value = analogRead(TEMPERATURE_SENSOR);
+
+    // Convert the sensor value to a temperature (in degrees Celsius)
+    //float temperature = (((value / 1024.0) * 5.0) - 0.5) * 100.0;
+
+    // Simpler, equivalent formula with only one floating-point division
+    float temperature = (value * 125L - 12800) / 256.0;
+
+    return temperature;
+}
+
+void temperature_actuator(float temperature) {
+    /* This function implements a simple debouncing mechanism to
+     * prevent the LED from flickering when the temperature is
+     * close to the threshold. The LED will only switch states
+     * when a temperature has been observed continuously above or
+     * below the threshold for a certain period of time, determined
+     * by the TEMPERATURE_DEBOUNCE_TIME constant.
+     */
+    static unsigned long threshold_time = 0;
+    static int switching_to = LOW;
+
+    unsigned long current_time = millis();
+
+    if (temperature > TEMPERATURE_THRESHOLD) {
+        if (switching_to == LOW) {
+            threshold_time = current_time;
+        }
+        switching_to = HIGH;
+    } else {
+        if (switching_to == HIGH) {
+            threshold_time = current_time;
+        }
+        switching_to = LOW;
+    }
+
+    if (current_time - threshold_time > TEMPERATURE_DEBOUNCE_TIME) {
+        digitalWrite(YELLOW_LED, switching_to);
+        threshold_time = current_time;
+    }
 }
