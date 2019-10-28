da0f6651b128d4cec99d4c8f984064b7e31ece76
Diogo Pereira <diogo.pereira@tecnico.ulisboa.pt>
2019-10-28 00:09:49 +0000

    Add lab3

---
+++ lab3/master/master.ino
@@ -0,0 +1,128 @@
+/* Applications and Computation for the Internet of Things 2019-2020
+ * 3rd Lab work: Network of Sensors and Actuators
+ *
+ * Group 11
+ * Diogo Pereira (58122)
+ * Pedro Carlos  (87556)
+ */
+
+#include <Wire.h>
+
+
+/* When DEBUG is defined, these values are printed to the serial port:
+ *     blink_period  brightness  temp_above_threshold  red_led_healthy
+ */
+#define DEBUG
+
+
+// Identification of the analog inputs (sensors)
+const byte TEMPERATURE_SENSOR = A0;
+const byte LIGHT_SENSOR       = A1;
+const byte POTENTIOMETER      = A3;
+
+// The range of the blinking period, depending on the potentiometer value
+const word MIN_BLINK_PERIOD = 200;
+const word MAX_BLINK_PERIOD = 2000;
+
+// The temperature above which the yellow LED turns on
+const float TEMPERATURE_THRESHOLD = 23.0;
+const float TEMPERATURE_NOISE_THRESHOLD = 0.5;
+
+const byte I2C_ADDRESS = 8;
+
+// Interval between LED health checks, in milliseconds
+const word HEALTH_CHECK_INTERVAL = 1000;
+
+
+void setup() {
+#ifdef DEBUG
+    Serial.begin(9600);
+#endif
+
+    Wire.begin();
+}
+
+void loop() {
+    // Each of the sensor reading functions takes ~112 us,
+    // which is pretty much all spent in analogRead().
+    int blink_period = rotation_sensor();
+    byte brightness = brightness_sensor();
+    float temperature = temperature_sensor();
+    bool temp_above_threshold = temperature_filter(temperature);
+
+    Wire.beginTransmission(I2C_ADDRESS);
+    Wire.write(highByte(blink_period));
+    Wire.write(lowByte(blink_period));
+    Wire.write(brightness);
+    Wire.write(byte(temp_above_threshold));
+    Wire.endTransmission();
+
+    // Detect failure of the red LED (open-circuit)
+    static byte red_led_healthy = true;
+    static unsigned long last_health_check = 0;
+    if (millis() - last_health_check >= HEALTH_CHECK_INTERVAL) {
+        Wire.requestFrom(I2C_ADDRESS, byte(1));
+        byte red_led_healthy = Wire.read();
+    }
+
+#ifdef DEBUG
+    Serial.print(blink_period);
+    Serial.print(" ");
+    Serial.print(brightness);
+    Serial.print(" ");
+    Serial.print(temperature);
+    Serial.print(" ");
+    Serial.print(temp_above_threshold);
+    Serial.print(" ");
+    Serial.println(red_led_healthy);
+#endif
+}
+
+
+int rotation_sensor() {
+    // Read the rotation value
+    int value = analogRead(POTENTIOMETER);
+
+    // Convert the rotation value to a blinking rate
+    int blink_period = map(value, 0, 1023, MIN_BLINK_PERIOD / 2, MAX_BLINK_PERIOD / 2);
+
+    return blink_period;
+}
+
+byte brightness_sensor() {
+    // Read the light sensor value
+    int value = analogRead(LIGHT_SENSOR);
+
+    // Map the light sensor value to a brightness level
+    byte brightness = map(value, 0, 1023, 0, 255);
+
+    return brightness;
+}
+
+float temperature_sensor() {
+    // Read the temperature sensor value
+    int value = analogRead(TEMPERATURE_SENSOR);
+
+    // Convert the sensor value to a temperature (in degrees Celsius)
+    //float temperature = (((value / 1024.0) * 5.0) - 0.5) * 100.0;
+
+    // Simpler, equivalent formula with only one floating-point division
+    float temperature = (value * 125L - 12800) / 256.0;
+
+    return temperature;
+}
+
+bool temperature_filter(float temperature) {
+    // This function implements a simple mechanism to filter sensor noise.
+
+    static bool above_threshold = false;
+
+    if (temperature > TEMPERATURE_THRESHOLD + TEMPERATURE_NOISE_THRESHOLD) {
+        above_threshold = true;
+    }
+    else if (temperature < TEMPERATURE_THRESHOLD - TEMPERATURE_NOISE_THRESHOLD) {
+        above_threshold = false;
+    }
+
+    return above_threshold;
+}
---
+++ lab3/slave/slave.ino
@@ -0,0 +1,133 @@
+/* Applications and Computation for the Internet of Things 2019-2020
+ * 3rd Lab work: Network of Sensors and Actuators
+ *
+ * Group 11
+ * Diogo Pereira (58122)
+ * Pedro Carlos  (87556)
+ */
+
+#include <Wire.h>
+
+
+/* When DEBUG is defined, these values are printed to the serial port:
+ *     blink_period  brightness  temp_above_threshold  red_led_healthy
+ */
+#define DEBUG
+
+
+// Identification of the digital outputs (actuators)
+const byte GREEN_LED  = 2;
+const byte RED_LED    = 3;
+const byte YELLOW_LED = 4;
+
+const byte I2C_ADDRESS = 8;
+
+// Interval between LED health checks, in milliseconds
+const word HEALTH_CHECK_INTERVAL = 1000;
+const byte RED_HEALTH_INPUT   = 12;
+
+
+int blink_period = 0;
+byte brightness = 0;
+byte temp_above_threshold = 0;
+bool red_led_healthy = true;
+
+
+void setup() {
+#ifdef DEBUG
+    Serial.begin(9600);
+#endif
+
+    // Initialize the outputs
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+    pinMode(YELLOW_LED, OUTPUT);
+    pinMode(RED_HEALTH_INPUT, INPUT);
+
+    Wire.begin(I2C_ADDRESS);
+    Wire.onReceive(data_received);
+    Wire.onRequest(request_received);
+}
+
+void loop() {
+    rotation_actuator(blink_period);   // 2-5 us
+    temperature_actuator(temp_above_threshold); // 5 us
+
+    // Run the health checker immediately before setting the
+    // brightness, so that the effect of the health checker
+    // (which temporarily sets the brightness to full) is not noticed.
+    static unsigned long last_health_check = 0;
+    if (millis() - last_health_check >= HEALTH_CHECK_INTERVAL) {
+        red_led_healthy = health_checker(RED_LED, RED_HEALTH_INPUT);
+    }
+    brightness_actuator(brightness);   // 8 us
+
+#ifdef DEBUG
+    Serial.print(blink_period);
+    Serial.print(" ");
+    Serial.print(brightness);
+    Serial.print(" ");
+    Serial.print(temp_above_threshold);
+    Serial.print(" ");
+    Serial.println(red_led_healthy);
+#endif
+}
+
+
+void data_received(int num_bytes) {
+    if (num_bytes == 4 || Wire.available() >= 4) {
+        blink_period = (Wire.read() << 8) | Wire.read();
+        brightness = Wire.read();
+        temp_above_threshold = Wire.read();
+    }
+}
+
+void request_received() {
+    Wire.write(byte(red_led_healthy));
+}
+
+bool health_checker(int output, int input) {
+    digitalWrite(output, HIGH);
+    if (digitalRead(input)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void rotation_actuator(int blink_period) {
+    // Current LED state
+    static byte led_state = LOW;
+    // Last time (in milliseconds) that the LED state was toggled
+    static unsigned long toggle_time = 0;
+
+    // Check if enough time has passed to toggle the LED state
+    unsigned long current_time = millis();
+    if (current_time > toggle_time + blink_period) {
+        // More time has passed than the blink rate; toggle LED state
+        led_state = (led_state == LOW) ? HIGH : LOW;
+        digitalWrite(GREEN_LED, led_state);
+
+        // Store the time that we toggled the state of the LED
+        toggle_time = current_time;
+    }
+}
+
+void brightness_actuator(int brightness) {
+    // Use PWM to control the LED brightness
+    analogWrite(RED_LED, brightness);
+}
+
+void temperature_actuator(byte temperature) {
+    if (temperature == 0) {
+        digitalWrite(YELLOW_LED, LOW);
+    }
+    else {
+        digitalWrite(YELLOW_LED, HIGH);
+    }
+}
